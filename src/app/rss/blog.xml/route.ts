0dea9b7288ffc66bb5f339486f4b2b3c48eaee42
MD Pabel <mdpabel385@gmail.com>
2025-08-02 01:49:11 +0600

    chore: static rss

--- src/app/rss/blog.xml/route.ts
+++ src/app/rss/blog.xml/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import { wordpress } from '@/lib/wordpress';
 import he from 'he'; // Import he for decoding
 
-export const dynamic = 'force-dynamic';
+export const dynamic = 'force-static';
 
 export async function GET() {
   const siteUrl = 'https://www.3zerodigital.com';
